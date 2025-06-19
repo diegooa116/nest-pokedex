116e3ddf7ec0dd1efb329c912877f071f7c3229b
dfoa12 <diegooa116@gmail.com>
2025-06-19 16:06:36 -0500

    POST - Recibir y validar la data

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,17 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
+
+export class CreatePokemonDto {
+
+    //isInt, isPositive, min 1
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    pokemonNumber:number;
+
+    // isString, Minlength 1
+    @IsString()
+    @MinLength(1)
+    name:string;
+
+
+}
