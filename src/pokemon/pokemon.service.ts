388f3b230f55a7df777cc562294ac81ec234dec7
dfoa12 <diegooa116@gmail.com>
2025-06-20 01:40:05 -0500

    Actualizar Pokemon en base de datos

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -20,10 +20,7 @@ export class PokemonService {
       const pokemon = await this.pokemonModel.create(createPokemonDto);
       return pokemon;
     } catch (error) {
-      if (error.code === 11000) {
-        throw new BadRequestException(`Pokemon exist in db ${JSON.stringify(error.keyValue)}`);
-      }
-      throw new InternalServerErrorException();
+      this.handleExceptions(error);
     }
 
   }
@@ -55,11 +52,31 @@ export class PokemonService {
     return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+
+    const pokemon = await this.findOne(term);
+
+    if (updatePokemonDto.name)
+      updatePokemonDto.name = updatePokemonDto.name.toLocaleLowerCase();
+    
+    try {
+      await pokemon.updateOne(updatePokemonDto); //* Devuelve el nuevo objeto {new:true}
+
+      return { ...pokemon.toJSON(), ...updatePokemonDto };
+
+    } catch (error) {
+      this.handleExceptions(error);
+    }
   }
 
   remove(id: number) {
     return `This action removes a #${id} pokemon`;
+  }
+
+  private handleExceptions(error: any) {
+    if (error.code === 11000) {
+      throw new BadRequestException(`Pokemon exist in db ${JSON.stringify(error.keyValue)}`);
+    }
+    throw new InternalServerErrorException();
   }
 }
