19659d2993ef8e96bd6a32a2497336abb19f4790
dfoa12 <diegooa116@gmail.com>
2025-06-22 19:19:02 -0500

    Realizar petición http desde Nest con axios

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
+
+  private readonly axios:AxiosInstance = axios;
   
-  executeSeed() {
-    return `Seed executed`;
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
+    
+    data.results.forEach(({name, url}) =>{
+
+      const segments = Number(url.split('/').at(-2));
+      
+    });
+
+    return data.results;
   }
 
 }
